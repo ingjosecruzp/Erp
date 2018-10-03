8240041832915c3a4220714b407ee474cddaa274
ingjosecruzp <ing.josecruzp@gmail.com>
2018-10-03 13:05:25 -0600

    Grid Conceptos

--- src/app/components/inventarios/conceptos/gridconceptos/gridconceptos.component.ts
+++ src/app/components/inventarios/conceptos/gridconceptos/gridconceptos.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Concepto } from '../../../../models/Inventarios/concepto';
+import { ConceptosService } from '../../../../services/inventarios/conceptos.service';
 
 @Component({
   selector: 'app-gridconceptos',
   templateUrl: './gridconceptos.component.html'
 })
 export class GridconceptosComponent implements OnInit {
+  conceptos: Concepto[] = [];
+  cols: any[];
+  selectedConcepto: Concepto;
 
-  constructor() { }
+  constructor(private WsConceptos: ConceptosService) {
+
+   }
 
   ngOnInit() {
+
+    this.cols = [
+      { field: 'Clave', header: 'Clave' },
+      { field: 'Nombre', header: 'Nombre' },
+      { field: 'Naturaleza', header: 'Naturaleza' },
+      { field: 'TipoConcepto._id', header: 'Tipo Concepto' },
+      { field: 'Predefinido', header: 'Predefinido' }
+    ];
+
   }
 
 }
