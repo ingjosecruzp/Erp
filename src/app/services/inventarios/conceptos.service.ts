98e556aae5963ebce9c0f2a958fd3501d269c2ba
ingjosecruzp <ing.josecruzp@gmail.com>
2018-10-03 12:05:21 -0600

    Servicio Conceptos

--- src/app/services/inventarios/conceptos.service.ts
+++ src/app/services/inventarios/conceptos.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { ServicesBase } from '../servicesBase';
+import { HttpClient } from '@angular/common/http';
+import { Concepto } from '../../models/Inventarios/concepto';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ConceptosService {
+export class ConceptosService implements ServicesBase {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+   }
+
+   getAll(): any {
+    return this.http.get<Concepto[]>('http://localhost:60493/Servicios/Inventarios/WcfConceptos.svc/');
+   }
+
+   save(item: any): any {
+    return this.http.post<Concepto>('http://localhost:60493/Servicios/Inventarios/WcfConceptos.svc/', item);
+ }
 }
