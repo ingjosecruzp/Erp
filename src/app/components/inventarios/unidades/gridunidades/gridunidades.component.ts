a5e7b0b3f5946da3bf15206c7af5002635ab1a47
TonyPlata <43586730+TonyPlata@users.noreply.github.com>
2018-10-03 15:04:26 -0500

    unidades

--- src/app/components/inventarios/unidades/gridunidades/gridunidades.component.ts
+++ src/app/components/inventarios/unidades/gridunidades/gridunidades.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Unidad } from '../../../../models/Generales/Unidad';
 import { UnidadService } from '../../../../services/inventarios/unidad.service';
+import { DialogService } from '../../../../services/dinamicos/dialog.service';
+import { FrmunidadesComponent } from '../frmunidades/frmunidades.component';
 
 @Component({
   selector: 'app-gridunidades',
@@ -11,13 +13,21 @@ export class GridunidadesComponent implements OnInit {
   unidades: Unidad[] = [];
   cols: any[];
   selectedConcepto: Unidad;
-  constructor( private WsUnidad: UnidadService) { }
+  constructor( private WsUnidad: UnidadService, public dialog: DialogService) { }
 
   ngOnInit( ) {
     this.cols = [
       { field: 'Nombre', header: 'Nombre' },
       { field: 'Abreviatura', header: 'Abreviatura' },
-  
+
     ];
+  }
+
+  add() {
+    const ref = this.dialog.open(FrmunidadesComponent , { 
+                  data: { message: 'I am a dynamic component inside of a dialog!'} });
+    /*ref.afterClosed.subscribe(result => {
+      console.log('Dialog closed', result);
+    });*/
   }
 }
