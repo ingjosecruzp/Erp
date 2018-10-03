7d3e8d27b726808a9997b022e1a68b9898904f12
TonyPlata <43586730+TonyPlata@users.noreply.github.com>
2018-10-03 14:31:47 -0500

    unidades

--- src/app/components/inventarios/unidades/gridunidades/gridunidades.component.ts
+++ src/app/components/inventarios/unidades/gridunidades/gridunidades.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Unidad } from '../../../../models/Generales/Unidad';
+import { UnidadService } from '../../../../services/inventarios/unidad.service';
 
 @Component({
   selector: 'app-gridunidades',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class GridunidadesComponent implements OnInit {
+  unidades: Unidad[] = [];
+  cols: any[];
+  selectedConcepto: Unidad;
+  constructor( private WsUnidad: UnidadService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit( ) {
+    this.cols = [
+      { field: 'Nombre', header: 'Nombre' },
+      { field: 'Abreviatura', header: 'Abreviatura' },
+  
+    ];
   }
-
 }
