dbdd590b57b0905f1013ba26dacb6fcb4bec9a77
TonyPlata <43586730+TonyPlata@users.noreply.github.com>
2018-10-04 15:29:48 -0500

    unidad

--- src/app/components/inventarios/unidades/frmunidades/frmunidades.component.ts
+++ src/app/components/inventarios/unidades/frmunidades/frmunidades.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { DialogConfig } from '../../../shared/dialog/dialog-config';
 import { DialogRef } from '../../../shared/dialog/dialog-ref';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-frmunidades',
   templateUrl: './frmunidades.component.html'
 })
 export class FrmunidadesComponent implements OnInit {
+  FrmDocumento: FormGroup;
   displayDialog: boolean;
-  constructor(public config: DialogConfig, public dialog: DialogRef) {this.displayDialog = true; }
+  constructor(public config: DialogConfig, public dialog: DialogRef, private fb: FormBuilder) {this.displayDialog = true; }
 
   ngOnInit() {
+    this.FrmDocumento = this.fb.group({
+      Nombre: ['', [Validators.required]],
+      Abreviatura: ['', [Validators.required]],
+   });
   }
 
 }
