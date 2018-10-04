f126366247165ff27f56fdf2e9722e7a233b728e
Germtz <ger.ms19@gmail.com>
2018-10-04 12:25:48 -0600

    pintado de formulario

--- src/app/components/inventarios/subgrupocomponentes/frmsubgrupocomponentes/frmsubgrupocomponentes.component.ts
+++ src/app/components/inventarios/subgrupocomponentes/frmsubgrupocomponentes/frmsubgrupocomponentes.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { DialogConfig } from '../../../shared/dialog/dialog-config';
 import { DialogRef } from '../../../shared/dialog/dialog-ref';
@@ -7,13 +8,17 @@ import { DialogRef } from '../../../shared/dialog/dialog-ref';
   templateUrl: './frmsubgrupocomponentes.component.html'
 })
 export class FrmsubgrupocomponentesComponent implements OnInit {
+  FrmSubgrupoComponente: FormGroup;
   displayDialog: boolean;
 
-  constructor(public config: DialogConfig, public dialog: DialogRef) {
+  constructor(public config: DialogConfig, public dialog: DialogRef, private fb: FormBuilder) {
     this.displayDialog = true;
    }
 
   ngOnInit() {
+    this.FrmSubgrupoComponente = this.fb.group({
+      Nombre: ['', [Validators.required]]
+   });
   }
 
 }
