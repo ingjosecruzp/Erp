a4778695229c5edde1e90b8bbb719a5499b8ec0e
Germtz <ger.ms19@gmail.com>
2018-10-05 13:10:39 -0600

    save frmsubgrupocomp

--- src/app/components/inventarios/subgrupocomponentes/frmsubgrupocomponentes/frmsubgrupocomponentes.component.ts
+++ src/app/components/inventarios/subgrupocomponentes/frmsubgrupocomponentes/frmsubgrupocomponentes.component.ts
@@ -4,6 +4,8 @@ import { DialogConfig } from '../../../shared/dialog/dialog-config';
 import { DialogRef } from '../../../shared/dialog/dialog-ref';
 import { GrupocomponenteService } from '../../../../services/inventarios/grupocomponente.service';
 import { GrupoComponente } from '../../../../models/Generales/grupocomponente';
+import { SubgrupoComponente } from '../../../../models/Generales/subgrupocompenente';
+import { SubgrupocomponenteService } from '../../../../services/inventarios/subgrupocomponente.service';
 
 @Component({
   selector: 'app-frmsubgrupocomponentes',
@@ -15,7 +17,7 @@ export class FrmsubgrupocomponentesComponent implements OnInit {
   GruposComponentes: GrupoComponente[];
 
   constructor(public config: DialogConfig, public dialog: DialogRef, private fb: FormBuilder, 
-              private WsGrupoComponentes: GrupocomponenteService) {
+              private WsGrupoComponentes: GrupocomponenteService, private WsSubgrupoComponentes: SubgrupocomponenteService) {
     this.displayDialog = true;
    }
 
@@ -34,6 +36,14 @@ export class FrmsubgrupocomponentesComponent implements OnInit {
   }
 
   save () {
-    console.log('FrmSubgrupoComponente');
+    // console.log('FrmSubgrupoComponente');
+      // console.log(item);
+    // console.log(JSON.stringify(item));
+    let item = new SubgrupoComponente(this.FrmSubgrupoComponente.value);
+ console.log(JSON.stringify(item));
+    this.WsSubgrupoComponentes.save(item).subscribe(data => {
+         console.log('Guardado');
+         console.log(data);
+        });
   }
 }
