c3e7ab2a5a8d4a84175f166de17a35bdbc7da0cd
Fernando Vazquez <fernando_keru1@hotmail.com>
2018-10-05 13:06:45 -0600

    GrupoComponente Guardar

--- src/app/components/inventarios/gruposcomponentes/frmgruposcomponentes/frmgruposcomponentes.component.ts
+++ src/app/components/inventarios/gruposcomponentes/frmgruposcomponentes/frmgruposcomponentes.component.ts
@@ -5,6 +5,7 @@ import { DialogRef } from '../../../shared/dialog/dialog-ref';
 import { TipoComponente } from '../../../../models/Generales/tipocomponente';
 import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 import { TipocomponenteService } from '../../../../services/inventarios/tipocomponente.service';
+import { GrupoComponente } from '../../../../models/Generales/grupocomponente';
 
 
 
@@ -19,7 +20,7 @@ export class FrmgruposcomponentesComponent implements OnInit {
   TipoComponente: TipoComponente[]; // se guarda en la base de datos al arrelgo
 
   // tslint:disable-next-line:max-line-length
-  constructor(public config: DialogConfig, public dialog: DialogRef, private WsTipoComponente: TipocomponenteService, private fb: FormBuilder ) {
+  constructor(public config: DialogConfig, public dialog: DialogRef, private WsGrupoComponente: GrupocomponenteService , private WsTipoComponente: TipocomponenteService, private fb: FormBuilder ) {
     this.displayDialog = true;
    }
 
@@ -43,7 +44,14 @@ export class FrmgruposcomponentesComponent implements OnInit {
 
 
   save () {
-    console.log('SaveGrupoCompnente');
+    let item = new GrupoComponente(this.FrmGrupoComponente.value);
+    console.log(item);
+    console.log(JSON.stringify(item));
+
+    this.WsGrupoComponente.save(item).subscribe(data => {
+         console.log('Guardado Grupo componente');
+         console.log(data);
+    });
   }
 
 
