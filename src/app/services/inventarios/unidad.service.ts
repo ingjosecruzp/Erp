1745852db9b1fb8fb0124ffa3fd82c2874756a5d
TonyPlata <43586730+TonyPlata@users.noreply.github.com>
2018-10-06 15:00:27 -0500

    unidad

--- src/app/services/inventarios/unidad.service.ts
+++ src/app/services/inventarios/unidad.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ServicesBase } from '../servicesBase';
 import { HttpClient } from '@angular/common/http';
+import { BaseService } from '../baseService';
 import { Unidad } from '../../models/Generales/Unidad';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UnidadService implements ServicesBase {
+export class UnidadService extends BaseService<Unidad> implements ServicesBase {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    super();
+    this.Ws = 'Servicios/Inventarios/WcfUnidades.svc';
+    this.httpWs = http;
+   }
 
   getAll(): any {
     return this.http.get<Unidad[]>('http://localhost:60493/Servicios/Inventarios/WcfUnidades.svc/');
@@ -17,4 +22,6 @@ export class UnidadService implements ServicesBase {
    save(item: any): any {
     return this.http.post<Unidad>('http://localhost:60493/Servicios/Inventarios/WcfUnidades.svc/', item);
  }
+
+ 
 }
