20134fbdacf7b5940a53a2bd7b73be70c90f934a
TonyPlata <43586730+TonyPlata@users.noreply.github.com>
2018-10-10 16:08:12 -0500

    Unidad

--- src/app/components/inventarios/unidades/frmunidades/frmunidades.component.ts
+++ src/app/components/inventarios/unidades/frmunidades/frmunidades.component.ts
@@ -5,46 +5,42 @@ import { DialogRef } from '../../../shared/dialog/dialog-ref';
 import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 import { Unidad } from '../../../../models/Generales/Unidad';
 import { UnidadService } from '../../../../services/inventarios/unidad.service';
+import { FrmBase } from 'src/app/components/frmbase';
 
 @Component({
   selector: 'app-frmunidades',
   templateUrl: './frmunidades.component.html'
 })
-export class FrmunidadesComponent implements OnInit {
+export class FrmunidadesComponent extends FrmBase<Unidad> implements OnInit {
   FrmDocumento: FormGroup;
   displayDialog: boolean;
   constructor(public config: DialogConfig, public dialog: DialogRef, private fb: FormBuilder, private WsUnindad: UnidadService) {
+    super();
     this.displayDialog = true;
-    console.log(config.data._id);
+    this.Ws = WsUnindad;
+    // console.log(config.data._id);
    }
 
   ngOnInit() {
-    this.FrmDocumento = this.fb.group({
+    this.FrmItem = this.fb.group({
       Nombre: ['', [Validators.required]],
       Abreviatura: ['', [Validators.required]],
    });
 
    if (this.config.data._id !== undefined) {
     this.WsUnindad.get(this.config.data._id).subscribe(data => {
+      this._id = data._id;
       let item = new Unidad(data);
-      console.log(item);
-      this.FrmDocumento.patchValue(item);
- 
+      this.FrmItem.patchValue(item);
+      console.log('Respuesta del servidor', data);
     });
   }
   }
-
-
 
   save () {
-
-    let item = new Unidad(this.FrmDocumento.value);
-
-    this.WsUnindad.save(item).subscribe(data => {
-         console.log('Guardado');
-         console.log(data);
-         this.FrmDocumento.reset();
-    });
+    this.item = new Unidad(this.FrmItem.value);
+    super.save();
   }
+
 
 }
