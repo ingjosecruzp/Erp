e899ebc921144ba409e03081f5ffa9671a3a059e
Fernando Vazquez <fernando_keru1@hotmail.com>
2018-10-10 12:38:36 -0600

    TipoComponente Guardar

--- src/app/components/inventarios/tipocomponentes/frmtipocomponentes/frmtipocomponentes.component.ts
+++ src/app/components/inventarios/tipocomponentes/frmtipocomponentes/frmtipocomponentes.component.ts
@@ -4,6 +4,8 @@ import { DialogRef } from '../../../shared/dialog/dialog-ref';
 import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 import { TipocomponenteService } from '../../../../services/inventarios/tipocomponente.service';
 import { TipoComponente } from '../../../../models/Generales/tipocomponente';
+import { IFrmBase } from '../../../ifrmbase';
+import { FrmBase } from '../../../frmbase';
 
 
 @Component({
@@ -11,39 +13,36 @@ import { TipoComponente } from '../../../../models/Generales/tipocomponente';
   templateUrl: './frmtipocomponentes.component.html',
 
 })
-export class FrmtipocomponentesComponent implements OnInit {
-  FrmTipoComponente: FormGroup;
+export class FrmtipocomponentesComponent extends FrmBase<TipoComponente> implements OnInit, IFrmBase {
+  FrmItem: FormGroup;
   displayDialog: boolean;
 
   constructor(public config: DialogConfig, public dialog: DialogRef,
                private fb: FormBuilder, private WsTipoComponente: TipocomponenteService) {
+    super();
     this.displayDialog = true;
+    this.Ws = WsTipoComponente;
    }
 
   ngOnInit() {
-    this.FrmTipoComponente = this.fb.group({
+    this.FrmItem = this.fb.group({
       Nombre: ['', [Validators.required]]
    });
    
    if (this.config.data._id !== undefined) {
     this.WsTipoComponente.get(this.config.data._id).subscribe(data => {
+        this._id = data._id;
         let item = new TipoComponente(data);
-         this.FrmTipoComponente.patchValue(item);
+        this.FrmItem.patchValue(item);
        console.log('Respuesta del servidor', data);
     });
   }
 
   }
 
-
   save () {
-    let item = new TipoComponente(this.FrmTipoComponente.value);
-
-    this.WsTipoComponente.save(item).subscribe(data => {
-         console.log('Guardado');
-         console.log(data);
-         this.FrmTipoComponente.reset();
-    });
+    this.item = new TipoComponente(this.FrmItem.value);
+    super.save();
   }
 
 }
