3ace24fbc4366c66872dc27ff9166db90722d522
TonyPlata <43586730+TonyPlata@users.noreply.github.com>
2018-10-12 12:04:24 -0500

    Cambios usuarios

--- src/app/components/shared/menubar/menubar.component.ts
+++ src/app/components/shared/menubar/menubar.component.ts
@@ -42,8 +42,8 @@ export class MenubarComponent implements OnInit {
                     {label: 'Grupos Componentes', icon: 'pi pi-fw fa fa-puzzle-piece', routerLink: ['/grupocompnentes']},
                     {label: 'Subgrupo Componentes', icon: 'pi pi-fw fa fa-object-group', routerLink: ['/subgrupocomponentes']},
                     {label: 'tipo Componentes', icon: 'pi pi-fw fa fa-object-group', routerLink: ['/tipocomponentes']},
-                    {label: 'Usuarios', icon: 'pi pi-fw fa fa-object-group', routerLink: ['/usuarios']},
-                    {label: 'Usuarios Rol', icon: 'pi pi-fw fa fa-object-group', routerLink: ['/usuariosrol']},
+                    {label: 'Usuarios', icon: 'pi pi-fw fa fa-object-group', routerLink: ['/usuario']},
+                    {label: 'Usuarios Rol', icon: 'pi pi-fw fa fa-object-group', routerLink: ['/usuariorol']},
                     {label: 'Refresh', icon: 'pi pi-fw pi pi-fw pi-refresh'},
                 ]
             }
