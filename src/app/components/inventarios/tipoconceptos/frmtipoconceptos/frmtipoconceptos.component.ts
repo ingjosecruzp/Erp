2cf6d577e79e846a386c1f557969384ca9577fe1
TonyPlata <43586730+TonyPlata@users.noreply.github.com>
2018-10-12 12:17:25 -0500

    Tipo conceptos

--- src/app/components/inventarios/tipoconceptos/frmtipoconceptos/frmtipoconceptos.component.ts
+++ src/app/components/inventarios/tipoconceptos/frmtipoconceptos/frmtipoconceptos.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FrmBase } from 'src/app/components/frmbase';
+import { TipoConcepto } from 'src/app/models/Inventarios/tipoconcepto';
+import { IFrmBase } from 'src/app/components/ifrmbase';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { DialogConfig } from 'src/app/components/shared/dialog/dialog-config';
+import { DialogRef } from 'src/app/components/shared/dialog/dialog-ref';
+import { TipoconceptoService } from 'src/app/services/inventarios/tipoconcepto.service';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-frmtipoconceptos',
   templateUrl: './frmtipoconceptos.component.html'
 })
-export class FrmtipoconceptosComponent implements OnInit {
+export class FrmtipoconceptosComponent extends FrmBase<TipoConcepto> implements OnInit, IFrmBase {
+  FrmItem: FormGroup;
+  displayDialog: boolean;
 
-  constructor() { }
+  constructor(public config: DialogConfig, public dialog: DialogRef,
+    private fb: FormBuilder, private WsTipoConcepto: TipoconceptoService,
+    private confirmationService: ConfirmationService) { 
+      super();
+      this.displayDialog = true;
+      this.Ws = WsTipoConcepto;
+    }
 
   ngOnInit() {
   }
--- src/app/components/inventarios/tipoconceptos/gridtipoconceptos/gridtipoconceptos.component.ts
+++ src/app/components/inventarios/tipoconceptos/gridtipoconceptos/gridtipoconceptos.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TipoConcepto } from '../../../../models/Inventarios/tipoconcepto';
+import { TipoconceptoService } from '../../../../services/inventarios/tipoconcepto.service';
+import { DialogService } from 'src/app/services/dinamicos/dialog.service';
+import { FrmtipoconceptosComponent } from '../frmtipoconceptos/frmtipoconceptos.component';
 
 @Component({
   selector: 'app-gridtipoconceptos',
@@ -10,9 +13,34 @@ export class GridtipoconceptosComponent implements OnInit {
  cols: any[];
   selectedConcepto: TipoConcepto;
 
-  constructor() { }
+  constructor(private WsTipoConcepto: TipoconceptoService,
+                      public dialog: DialogService) { }
 
   ngOnInit() {
+    this.cols = [
+      { field: 'Nombre', header: 'Nombre' }
+    ];
   }
+
+  refresh() {
+    this.WsTipoConcepto.getAll().subscribe(data => {
+      this.WsTipoConcepto = data;
+     });
+  }
+
+  open(item: TipoConcepto) {
+    const ref = this.dialog.open(FrmtipoconceptosComponent, { 
+      data: { _id: item._id} });
+  }
+
+  add() {
+    const ref = this.dialog.open(FrmtipoconceptosComponent, { 
+                  data: { message: 'I am a dynamic component inside of a dialog!'} });
+  
+    /*ref.afterClosed.subscribe(result => {
+      console.log('Dialog closed', result);
+    });*/
+  }
+
 
 }
