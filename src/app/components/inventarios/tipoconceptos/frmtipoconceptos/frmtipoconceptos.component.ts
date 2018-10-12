e527265f429dd37bd688b0d4aee805c3d121de41
TonyPlata <43586730+TonyPlata@users.noreply.github.com>
2018-10-12 11:39:19 -0500

    cambios

--- src/app/components/inventarios/tipoconceptos/frmtipoconceptos/frmtipoconceptos.component.ts
+++ src/app/components/inventarios/tipoconceptos/frmtipoconceptos/frmtipoconceptos.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-frmtipoconceptos',
-  templateUrl: './frmtipoconceptos.component.html',
-  styleUrls: ['./frmtipoconceptos.component.css']
+  templateUrl: './frmtipoconceptos.component.html'
 })
 export class FrmtipoconceptosComponent implements OnInit {
 
