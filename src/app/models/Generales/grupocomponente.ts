1c859de67082d7631da837f71aaa2667ec78d107
Fernando Vazquez <fernando_keru1@hotmail.com>
2018-10-15 13:53:31 -0600

    Almacenes

--- src/app/models/Generales/grupocomponente.ts
+++ src/app/models/Generales/grupocomponente.ts
@@ -3,6 +3,7 @@ export class GrupoComponente {
     _id: string;
     Nombre: string;
     TipoComponente: TipoComponente;
+    
     public constructor(init?: Partial<GrupoComponente>) {
         Object.assign(this, init);
     }
