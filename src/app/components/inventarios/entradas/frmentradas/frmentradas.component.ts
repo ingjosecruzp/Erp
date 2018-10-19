0739c63c76b92b35929d0f192928ee48c3608a12
Germtz <ger.ms19@gmail.com>
2018-10-19 12:47:37 -0600

    entradas

---
+++ src/app/components/inventarios/entradas/frmentradas/frmentradas.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frmentradas',
+  templateUrl: './frmentradas.component.html'
+})
+export class FrmentradasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
