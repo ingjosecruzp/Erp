cddee43ce0ca695c170dfccd5f44ca7e760128bd
Germtz <ger.ms19@gmail.com>
2018-10-20 10:22:48 -0600

    combos en entradas

--- src/app/components/inventarios/entradas/frmentradas/frmentradas.component.ts
+++ src/app/components/inventarios/entradas/frmentradas/frmentradas.component.ts
@@ -7,6 +7,11 @@ import { IFrmBase } from '../../../ifrmbase';
 import { FrmBase } from '../../../frmbase';
 import {ConfirmationService} from 'primeng/api';
 import { Entradas } from '../../../../models/Inventarios/entradas';
+import { Concepto } from '../../../../models/Inventarios/concepto';
+import { Almacen } from '../../../../models/Inventarios/almacen';
+import { TipoconceptoService } from '../../../../services/inventarios/tipoconcepto.service';
+import { AlmacenService } from '../../../../services/inventarios/almacen.service';
+
 
 @Component({
   selector: 'app-frmentradas',
@@ -16,10 +21,15 @@ import { Entradas } from '../../../../models/Inventarios/entradas';
 export class FrmentradasComponent extends FrmBase<Entradas> implements OnInit, IFrmBase {
   FrmEntradas: FormGroup;
   displayDialog: boolean;
+  TipoConceptos: Concepto[];
+  Almacen: Almacen[];
+
 
   constructor(public config: DialogConfig,
     public dialog: DialogRef,
     private fb: FormBuilder,
+    private WsConcepto: TipoconceptoService,
+    private WsAlmacenes: AlmacenService,
     private confirmationService: ConfirmationService) {
       super();
     this.displayDialog = true;
@@ -28,8 +38,12 @@ export class FrmentradasComponent extends FrmBase<Entradas> implements OnInit, I
 
   ngOnInit() {
     this.FrmItem = this.fb.group({
-      Nombre: [null, [Validators.required]],
-      TipoComponente: [null, [Validators.required]]
+      fecha: [null, [Validators.required]],
+      folio: [null, [Validators.required]],
+      TipoConceptos: [null, [Validators.required]],
+      Almacen: [null, [Validators.required]],
+      descripcion: [null, [Validators.required]]
+
    });
   }
 
--- src/app/services/inventarios/entradas.service.ts
+++ src/app/services/inventarios/entradas.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { ServicesBase } from '../servicesBase';
+import { HttpClient } from '@angular/common/http';
+import { BaseService } from '../baseService';
+import { Entradas } from '../../models/Inventarios/entradas';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class EntradasService {
+export class EntradasService extends BaseService<Entradas> implements ServicesBase {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    super();
+    this.Ws = 'Servicios/Inventarios/WcfSubgruposComponentes.svc';
+    this.httpWs = http;
+  }
+
+  getAll() {
+    return this.http.get<Entradas[]>('http://localhost:60493/Servicios/Inventarios/WcfSubgruposComponentes.svc/');
+  }
+  save(item: any) {
+    return this.http.post<Entradas>('http://localhost:60493/Servicios/Inventarios/WcfSubgruposComponentes.svc/', item);
+  }
+
 }
+
