507d5c15bb7df9febc4b82c2237b50b1bce8b75a
Germtz <ger.ms19@gmail.com>
2018-11-23 17:09:53 -0700

    pureza grid y frm

---
+++ src/app/components/inventarios/pureza/frmpureza/frmpureza.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frmpureza',
+  templateUrl: './frmpureza.component.html'
+})
+export class FrmpurezaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/inventarios/pureza/gridpureza/gridpureza.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gridpureza',
+  templateUrl: './gridpureza.component.html'
+})
+export class GridpurezaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
